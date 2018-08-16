1568950b51725d59202c3099a37de86df8253490
Akihiko Koizuka <koizuka@gmail.com>
2018-08-16 18:48:22 +0900

    v0.1.20180816-1d

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,11 +1,10 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '0.1.20180809-1',
-  title: '0.1.20180809-1',
+  version: '0.1.20180816-1d',
+  title: '0.1.20180816-1d',
   notes: [
-    '主な更新内容',
-    ' * #10 Webページソースの挙動が怪しかったため、モジュールのバージョンを Streamlabs-obs に追従し、obs-studio-node v0.0.48 から v0.0.47 にロールバック',
-    ' * #6 (機能影響なし)ソース中の import モジュール名の大文字小文字修正'
+    'ニコ生配信の場合の設定最適化機能を追加',
+    '更新内容のお知らせ表示追加'
   ]
 };
