155f3211a815d6b94677d49ed9c27b3578f6f44d
Akihiko Koizuka <koizuka@gmail.com>
2018-08-27 15:59:58 +0900

    note.ts 0.1.20180827-1d

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,10 +1,14 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '0.1.20180824-1d',
-  title: '0.1.20180824-1d',
+  version: '0.1.20180827-1d',
+  title: '0.1.20180827-1d',
   notes: [
-    'ソースのプロパティに表示されるlabelの横幅を調整 (#42)',
-    'Updaterのデザインが崩れていたのを集成 (#40)'
+    'アイテムのリサイズ時にディスプレイ倍率が1以外の環境でずれる問題を修正 (#47) by koizuka',
+    '頂点にあるリサイズボックスでもクロップ可能にする (#45) by berlysia',
+    'ニコ生で複数の番組配信候補があるときに選択する機能を追加 (#44) by koizuka',
+    'リリーススクリプトから github release にuploadする (#48) by koizuka',
+    'インストール時に表示する規約の更新 (#43) by koizuka',
+    'Download pageのURLサブドメイン変更 (#49) by Makoto-Sasahara'
   ]
 };
