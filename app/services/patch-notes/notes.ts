5a09dff2318b2564715b5e45a5ea432458064500
Akihiko Koizuka <koizuka@gmail.com>
2018-08-28 15:57:12 +0900

    note.ts 0.1.20180828-1d

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,14 +1,9 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '0.1.20180827-1d',
-  title: '0.1.20180827-1d',
+  version: '0.1.20180828-1d',
+  title: '0.1.20180828-1d',
   notes: [
-    'アイテムのリサイズ時にディスプレイ倍率が1以外の環境でずれる問題を修正 (#47) by koizuka',
-    '頂点にあるリサイズボックスでもクロップ可能にする (#45) by berlysia',
-    'ニコ生で複数の番組配信候補があるときに選択する機能を追加 (#44) by koizuka',
-    'リリーススクリプトから github release にuploadする (#48) by koizuka',
-    'インストール時に表示する規約の更新 (#43) by koizuka',
-    'Download pageのURLサブドメイン変更 (#49) by Makoto-Sasahara'
+    'Electron upgrade to 2.0.8 (#52)'
   ]
 };
