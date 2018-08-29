44c7cb9310c22833dff0ce9431c1086593af8ba9
Akihiko Koizuka <koizuka@gmail.com>
2018-08-29 20:52:45 +0900

    note.ts 0.1.20180829-1d

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,9 +1,12 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '0.1.20180828-1d',
-  title: '0.1.20180828-1d',
+  version: '0.1.20180829-1d',
+  title: '0.1.20180829-1d',
   notes: [
-    'Electron upgrade to 2.0.8 (#52)'
+    'sentryにsourcemapをアップロードする簡単なスクリプトを追加 (#61) by koizuka',
+    '番組情報から取得したbitrateを最適化に利用する (#53) by pocketberserker',
+    '高度な音声設定の音声遅延を0未満にしようとするとエラーになる問題を修正 (#57) by pocketberserker',
+    'platformがundefinedのときにエラーになる問題を修正 (#56) by pocketberserker'
   ]
 };
