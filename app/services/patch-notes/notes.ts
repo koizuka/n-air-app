c66a9f8583d66f807a4c072d7580f56f6016e193
Akihiko Koizuka <koizuka@gmail.com>
2018-09-01 00:41:29 +0900

    note.ts 0.1.20180901-1d

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,11 +1,14 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '0.1.20180830-1d',
-  title: '0.1.20180830-1d',
+  version: '0.1.20180901-1d',
+  title: '0.1.20180901-1d',
   notes: [
-    'Fix README (Visual Studioコード -> Visual Studio Code) (#65) by rinsuki',
-    '修正: 未ログインで放送するときには視聴者数更新処理を無効化 (#63) by koizuka',
-    '追加: インターレース解除機能 (#54) by nullkal'
+    'release でelectron-updater用に blockmap 追加してみた',
+    'upgrade raven-js (sentry.io) to latest(3.26.4) (#68) by koizuka',
+    '修正: シーンエディタ上で右クリックしたときに対象が未選択だった場合、対象単体が選択されるように変更 (#64) by koizuka',
+    '修正: 設定変更時に存在しないデバイスをソースから取り除く (#67) by pocketberserker',
+    '追加: ディスプレイキャプチャを画面上でクロップする (#58) by berlysia',
+    '修正: ソースアイテムを削除したらそのプロジェクターウィンドウも閉じる (#66) by koizuka'
   ]
 };
