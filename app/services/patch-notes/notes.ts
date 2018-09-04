d78b7f44205db5feaa090f6eea50ee0d01a40a47
Akihiko Koizuka <koizuka@gmail.com>
2018-09-04 17:27:39 +0900

    note.ts 0.1.20180904-1d

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,9 +1,11 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '0.1.20180903-1d',
-  title: '0.1.20180903-1d',
+  version: '0.1.20180904-1d',
+  title: '0.1.20180904-1d',
   notes: [
-    'ディスプレイキャプチャの領域指定機能の文言修正 (#71) by yushinakatani'
+    '修正: アップデータの差分更新が動くようにする (#74) by koizuka',
+    '修正: (main.jsの再起動処理で引数が継承されていなかった(ただし使ってない) (#73) by koizuka',
+    '修正 (社内版): build.productNameを N Air(社内版) に変更(アプリタイトルやインストーラの文言が変わる)'
   ]
 };
