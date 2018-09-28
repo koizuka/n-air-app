4d3a082674311476e41a788dc5f70c0ec791bc77
Akihiko Koizuka <koizuka@gmail.com>
2018-09-28 20:53:36 +0900

    v0.1.20180928-2d

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,10 +1,10 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '0.1.20180928-1d',
-  title: '0.1.20180928-1d',
+  version: '0.1.20180928-2d',
+  title: '0.1.20180928-2d',
   notes: [
-    '修正: アップデータで「更新内容」ラベルが二つでていたのを修正(アップデート後に反映)',
-    '修正: リリースアップロード失敗時に手でSentryにデバッグシンボル情報を送信するときのスクリプト bin/upload-to-sentry.sh で SENTRY_PROJECT指定が効いてなかったのを修正'
+    'アップデートスキップ画面確認用の更新です。',
+    'v0.1.20180928-1d から実行することで確認できます。'
   ]
 };
