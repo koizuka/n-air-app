a826be427c16aa6f547d86dd60a41efb6035390d
Akihiko Koizuka <koizuka@gmail.com>
2018-09-28 13:10:04 +0900

    v0.1.20180928-1d

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,10 +1,10 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '0.1.20180927-1d',
-  title: '0.1.20180927-1d',
+  version: '0.1.20180928-1d',
+  title: '0.1.20180928-1d',
   notes: [
-    'アップデータを発動させるために追加社内リリース',
-    '追加: 設定画面に問い合わせ用キャッシュID表示を追加'
+    '修正: アップデータで「更新内容」ラベルが二つでていたのを修正(アップデート後に反映)',
+    '修正: リリースアップロード失敗時に手でSentryにデバッグシンボル情報を送信するときのスクリプト bin/upload-to-sentry.sh で SENTRY_PROJECT指定が効いてなかったのを修正'
   ]
 };
