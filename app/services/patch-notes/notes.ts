dd6f318f7f7b1f05dc2c625ec9bb58e43336d20f
takayamaki <takayamaki@takayamaki.jp>
2018-10-12 21:20:50 +0900

    v1.0.20181012-2d

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,10 +1,9 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '1.0.20181012-1d',
-  title: '1.0.20181012-1d',
+  version: '1.0.20181012-2d',
+  title: '1.0.20181012-2d',
   notes: [
-    '修正: サイドチェーンのトリガー一覧にソースの名前を表示する (#122) by berlysia',
-    '修正: モーダル部分のスペーシングをシンプルにする (#104) by berlysia'
+    'electron-builderのオプションにuseMultipleRangeRequestが必要だったため追加'
   ]
 };
