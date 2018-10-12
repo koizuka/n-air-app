8753e90c0ff53ac012618efedb6ae726942755a0
takayamaki <takayamaki@takayamaki.jp>
2018-10-12 22:58:33 +0900

    v1.0.20181012-3d

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,9 +1,9 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '1.0.20181012-2d',
-  title: '1.0.20181012-2d',
+  version: '1.0.20181012-3d',
+  title: '1.0.20181012-3d',
   notes: [
-    'electron-builderのオプションにuseMultipleRangeRequestが必要だったため追加'
+    '音声系ソースの名前を翻訳 (#124) by berlysia'
   ]
 };
