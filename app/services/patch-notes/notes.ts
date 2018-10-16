0c25131d7bf21b8bc722d2d01bb76ff47aa27e47
Akihiko Koizuka <koizuka@gmail.com>
2018-10-16 15:20:50 +0900

    v1.0.20181016-1d

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,9 +1,11 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '1.0.20181012-3d',
-  title: '1.0.20181012-3d',
+  version: '1.0.20181016-1d',
+  title: '1.0.20181016-1d',
   notes: [
-    '音声系ソースの名前を翻訳 (#124) by berlysia'
+    '変更: ニコニコ配信最適化の低遅延強化 (#84) by koizuka',
+    '',
+    '変更: インストーラのダウンロード元を自前のs3に変更 (社内)'
   ]
 };
