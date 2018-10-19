a93adddedd13dbfa50f849ae7421f1bb61ed7cd6
Akihiko Koizuka <koizuka@gmail.com>
2018-10-19 16:13:53 +0900

    v1.0.20181019-1d

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,11 +1,11 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '1.0.20181016-1d',
-  title: '1.0.20181016-1d',
+  version: '1.0.20181019-1d',
+  title: '1.0.20181019-1d',
   notes: [
-    '変更: ニコニコ配信最適化の低遅延強化 (#84) by koizuka',
-    '',
-    '変更: インストーラのダウンロード元を自前のs3に変更 (社内)'
+    '修正:最適化スキップ時にEncoderがQSVだとその設定が初期値に戻ってしまう (#162) by koizuka',
+    '修正:最適化項目にビットレート制御(CBR)を追加 / 最適化ウィンドウの高さ自動計算 (#170) by koizuka',
+    '修正:最適化項目にフレームレートの指定方法も追加 (#169) by koizuka'
   ]
 };
