e0561aaf8489cce607842c85979a97d7c973630b
Akihiko Koizuka <koizuka@gmail.com>
2018-11-06 14:12:42 +0900

    v1.0.20181106-1d

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,10 +1,9 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '1.0.20181105-1d',
-  title: '1.0.20181105-1d',
+  version: '1.0.20181106-1d',
+  title: '1.0.20181106-1d',
   notes: [
-    '追加: CIでユニットテストを実行＆StreamingServiceにユニットテストを用意 (#192) by berlysia',
-    '修正: getplayerstatusのHTTPエラーおよびネットワークエラーを間引く(エラー調査用) (#181) by berlysia'
+    '修正: リリーススクリプトで GitHubにアップロード時の切断エラーをリトライする (#194) by koizuka'
   ]
 };
