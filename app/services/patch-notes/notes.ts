9863aca4b84577f26f455f95f7588ba8838818eb
Akihiko Koizuka <koizuka@gmail.com>
2018-11-27 17:18:45 +0900

    v1.0.20181127-2d

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,13 +1,9 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '1.0.20181127-1d',
-  title: '1.0.20181127-1d',
+  version: '1.0.20181127-2d',
+  title: '1.0.20181127-2d',
   notes: [
-    '追加: シーンコレクションのmanifestバックアップ機構を追加 (#205) by berlysia',
-    '変更: ダイアログの配色を調整/スクロールバーの幅を少し太くした (#204) by yusukess',
-    '修正: 「映像キャプチャデバイス」という名前を「Webカメラ/映像入力機器」に変更 (#199) by yushinakatani',
-    'better sentry log message of optimization status (#202) by koizuka',
-    '文言修正：映像キャプチャデバイス (#201) by yushinakatani'
+    '追加: パフォーマンス情報の定期取得を無効にする設定を追加 (#206) by berlysia'
   ]
 };
