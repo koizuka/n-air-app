e4b68e9d27f98551d51d7f1a1b36ac5c73a7cda7
Akihiko Koizuka <koizuka@gmail.com>
2018-11-27 15:01:07 +0900

    v1.0.20181127-1d

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,16 +1,13 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '1.0.20181106-1',
-  title: '1.0.20181106-1',
+  version: '1.0.20181127-1d',
+  title: '1.0.20181127-1d',
   notes: [
-    '配信安定性向上を目的とした更新です。',
-    '修正: ストリームキーエラーのメッセージでニコニコログイン案内を追加 (#184)',
-    '修正: ニコニコ生放送最適化の設定書き込みを確実にするためにリトライ (#191)',
-    '修正: ログイン中に配信を開始して番組がないと返ってきたら1回リトライ (#115)',
-    '変更: 音声ミュート中であることが気づきやすいようにミュートボタンを強調 (#190)',
-    '修正: SceneCollectionのmanifestファイル読み込み失敗を収集(エラー調査用) (#189)',
-    '修正: getplayerstatusのHTTPエラーおよびネットワークエラーを間引く(エラー調査用) (#181)',
-    '追加: CIでユニットテストを実行＆StreamingServiceにユニットテストを用意 (#192)'
+    '追加: シーンコレクションのmanifestバックアップ機構を追加 (#205) by berlysia',
+    '変更: ダイアログの配色を調整/スクロールバーの幅を少し太くした (#204) by yusukess',
+    '修正: 「映像キャプチャデバイス」という名前を「Webカメラ/映像入力機器」に変更 (#199) by yushinakatani',
+    'better sentry log message of optimization status (#202) by koizuka',
+    '文言修正：映像キャプチャデバイス (#201) by yushinakatani'
   ]
 };
