3719a2cd9de7c00ef91273c9165bcbb4e4f022b8
berlysia <berlysia@gmail.com>
2019-02-07 20:40:26 +0900

    v1.0.20190207-1d

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,9 +1,15 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '1.0.20181129-2d',
-  title: '1.0.20181129-2d',
+  version: '1.0.20190207-1d',
+  title: '1.0.20190207-1d',
   notes: [
-    'Revert "Merge pull request #199 from n-air-app/yushinakatani-patch-1-1" (#208) by berlysia'
+    '変更: プレビューエリアの背景色を変更 (#216) by yusukess',
+    '変更: 起動時のloadingのSVGアニメーションの改善 (#209) by hiroshi-takezawa',
+    '修正: 使われていないNameSceneCollection.vueを削除 (#224) by takayamaki',
+    '改善: childWindowが画面内に出現することを保証する (#230) by berlysia',
+    '改善: mainWindowの画面内判定を修正 (#231) by berlysia',
+    '改善: 編集可能要素にコンテキストメニューを表示する (#229) by berlysia',
+    '改善：サブウインドウがメインウインドウの裏に隠れてしまわないように変更 (#225) by takayamaki'
   ]
 };
