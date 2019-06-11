4c3050d0518e4f2f034b034a08bc908bb26ff3b0
berlysia <berlysia@gmail.com>
2019-06-11 15:21:22 +0900

    v1.0.20190611-1d

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,15 +1,15 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '1.0.20190207-1d',
-  title: '1.0.20190207-1d',
+  version: '1.0.20190611-1d',
+  title: '1.0.20190611-1d',
   notes: [
-    '変更: プレビューエリアの背景色を変更 (#216) by yusukess',
-    '変更: 起動時のloadingのSVGアニメーションの改善 (#209) by hiroshi-takezawa',
-    '修正: 使われていないNameSceneCollection.vueを削除 (#224) by takayamaki',
-    '改善: childWindowが画面内に出現することを保証する (#230) by berlysia',
-    '改善: mainWindowの画面内判定を修正 (#231) by berlysia',
-    '改善: 編集可能要素にコンテキストメニューを表示する (#229) by berlysia',
-    '改善：サブウインドウがメインウインドウの裏に隠れてしまわないように変更 (#225) by takayamaki'
+    '追加: 最適化ダイアログにハードウェアエンコーダを使うかどうかのオプションを追加 (#315) by koizuka',
+    '変更: アップデーターのボタン文言を簡潔にする (#312) by berlysia',
+    '修正: v-forのkeyを指定 (#313) by berlysia',
+    'Issue/PRのテンプレートを更新 (#305) by berlysia',
+    'sentryのエラーログ内容にCPU, GPU, メモリ, OS情報を追加 (#299) by takayamaki',
+    '更新: ビルド回りの依存ファイルを更新 (#256) by berlysia',
+    '番組URLをクリップボードへコピーする機能を番組情報パネルに追加 (#301) by takayamaki'
   ]
 };
