1617268181d20c86b7b91f19145176ab3c6ae66e
berlysia <berlysia@gmail.com>
2019-06-12 18:01:17 +0900

    v1.0.20190612-1d

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,15 +1,9 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '1.0.20190611-1d',
-  title: '1.0.20190611-1d',
+  version: '1.0.20190612-1d',
+  title: '1.0.20190612-1d',
   notes: [
-    '追加: 最適化ダイアログにハードウェアエンコーダを使うかどうかのオプションを追加 (#315) by koizuka',
-    '変更: アップデーターのボタン文言を簡潔にする (#312) by berlysia',
-    '修正: v-forのkeyを指定 (#313) by berlysia',
-    'Issue/PRのテンプレートを更新 (#305) by berlysia',
-    'sentryのエラーログ内容にCPU, GPU, メモリ, OS情報を追加 (#299) by takayamaki',
-    '更新: ビルド回りの依存ファイルを更新 (#256) by berlysia',
-    '番組URLをクリップボードへコピーする機能を番組情報パネルに追加 (#301) by takayamaki'
+    '最適化ダイアログから「今後、このダイアログを表示しない」 チェックボックスが消えていたので復活 (#316) by koizuka'
   ]
 };
