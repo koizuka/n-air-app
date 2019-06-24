9d00a0e894594dd3545554fcc420b2c7ce7bcc81
berlysia <berlysia@gmail.com>
2019-06-24 21:28:49 +0900

    v1.0.20190624-3d

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,12 +1,9 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '1.0.20190621-1d',
-  title: '1.0.20190621-1d',
+  version: '1.0.20190624-3d',
+  title: '1.0.20190624-3d',
   notes: [
-    'インフラの構成変更に伴いインストーラなどのS3へのアップロードパスを変更 (#320) by takayamaki',
-    'リリーススクリプトの依存更新: update @octokit/rest to 16.28.1 (#319) by koizuka',
-    '収納ボタンの見た目を修正 (#318) by yusukess',
-    '非ログイン状態のときにUserExtraContextが正しく取得出来ていなかった問題を修正 (#317) by takayamaki'
+    '修正: リリーススクリプトのリネーム漏れに対処 (#322) by berlysia'
   ]
 };
