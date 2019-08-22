f5d46510356c8a682d37a37eacd6577a91d260e4
Akihiko Koizuka <koizuka@dwango.co.jp>
2019-08-22 18:11:46 +0900

    v1.0.20190822-1d

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,12 +1,10 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '1.0.20190820-2d',
-  title: '1.0.20190820-2d',
+  version: '1.0.20190822-1d',
+  title: '1.0.20190822-1d',
   notes: [
-    '追加: 400エラーで番組延長失敗時のエラーメッセージを親切にする (#326) by berlysia',
-    '追加: シリーズに自動リンク対応 (#325) by berlysia',
-    '変更: ニコ生以外の画面に遷移したら既定のブラウザで開いてwebviewは閉じる (#328) by berlysia',
-    '修正: 番組作成画面から新しいウィンドウを開こうとしたら既定ブラウザで開く (#327) by berlysia'
+    'リリーススクリプトで使うinquirerのバージョンを4から6に上げる (#331) by koizuka',
+    '更新: sl-vue-treeの参照を更新 (#332) by berlysia'
   ]
 };
